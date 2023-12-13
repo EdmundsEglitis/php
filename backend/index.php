4640e3eaf11082d58002401fd1265c8dd108adec
EdmundsEglitis <edmunds3011@gmail.com>
2023-12-13 20:14:07 +0200

    good push

--- backend/index.php
+++ backend/index.php
@@ -2,12 +2,19 @@
 include "Circle.php";
 include "Rectangle.php";
 header("Access-Control-Allow-Origin: *");
-$data = ["message"=> "Hi from backend"];
+//$data = ["message"=> "Hi from backend"];
+//echo json_encode($data);
+
+$circle = new Circle(200,"yellow");
+$rectangle = new Rectangle(400,800,"green");
+
+$data =  ["circleRadius" => $circle->radius,
+          "circleArea" => $circle->calculateArea(),
+          "circleColor" => $circle->color,
+          "rectangleHeight" => $rectangle->height,
+          "rectangleWidth" => $rectangle->width,
+          "rectangleArea" => $rectangle->calculateArea(),
+          "rectangleColor" => $rectangle->color
+];
 echo json_encode($data);
-
-$circle = new Circle(8,"yellow");
-$rectangle = new Rectangle(8,16,"green");
-
-echo $circle->calculateArea();
-
 ?>
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,47 @@
-import { useEffect } from "react";
+
+
+import React, { useEffect, useState } from 'react';
 
 function App() {
-    useEffect (()=> {
-    async function getData(){
+  const [shapesData, setShapesData] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
       const response = await fetch("http://localhost:80");
       const data = await response.json();
+      setShapesData(data);
       console.log(data);
     }
+
     getData();
-  },[]);
+  }, []);
+
   return (
     <div className="App">
-        <h1>HI</h1>
+      <h1>Shapes</h1>
+
+{shapesData && (
+  <div>
+    <div>
+      <h2>Circle</h2>
+      <div className="circle" style={{ "--radius": `${shapesData.circleRadius}px` }}>
+        <p>Radius: {shapesData.circleRadius}</p>
+        <p>Area: {shapesData.circleArea}</p>
+        <p>Color: {shapesData.circleColor}</p>
+      </div>
+    </div>
+
+    <div>
+      <h2>Rectangle</h2>
+      <div className="rectangle" style={{ "--width": `${shapesData.rectangleWidth}px`, "--height": `${shapesData.rectangleHeight}px` }}>
+        <p>Height: {shapesData.rectangleHeight}</p>
+        <p>Width: {shapesData.rectangleWidth}</p>
+        <p>Area: {shapesData.rectangleArea}</p>
+        <p>Color: {shapesData.rectangleColor}</p>
+      </div>
+    </div>
+  </div>
+)}
     </div>
   );
 }
